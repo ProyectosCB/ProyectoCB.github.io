1ca91af73cbf8118b1981fee74d9ebf2e7ac1717
ProyectosCB <127132259+ProyectosCB@users.noreply.github.com>
2023-03-06 14:39:29 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+//Variables
+    document.getElementById("boton").onclick = function (){
+    document.getElementById("demo").innerHTML="-Estudios secundarios completos -Curso de operador de PC -Empleado bancario";  
+}
+          
+
+    
+
+
+
+
+
+
+//funciones
+ 
+   
+boton.addEventListener('click',ExperienciaLaboral);
+document.addEventListener('DOMContentLoaded',ExperienciaLaboral)
