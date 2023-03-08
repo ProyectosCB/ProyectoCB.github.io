0d71b45d0bb21888a0daf63448eeec075d2ade7f
ProyectosCB <127132259+ProyectosCB@users.noreply.github.com>
2023-03-08 16:53:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 //Variables
     document.getElementById("boton").onclick = function (){
-    document.getElementById("demo").innerHTML="-Estudios secundarios completos -Curso de operador de PC -Empleado bancario";  
-}
-          
-
+    document.getElementById("demo").innerHTML="Estudios secundarios completos";
     
+}
 
+    document.getElementById("boton1").onclick = function (){
+    document.getElementById("demo").innerHTML="Preventista - Empleado bancario";
+}           
+
+    document.getElementById("boton2").onclick = function (){
+    document.getElementById("demo").innerHTML="Formulario de Contacto";
+}
+   
 
 
 
 
 
 //funciones
- 
    
-boton.addEventListener('click',ExperienciaLaboral);
-document.addEventListener('DOMContentLoaded',ExperienciaLaboral)
+boton.addEventListener('click', ExperienciaLaboral);
+document.addEventListener('DOMContentLoaded',ExperienciaLaboral); 
+
+boton.addEventListener('click',Títulos);
+document.addEventListener('DOMContentLoaded',Títulos);
+
+boton.addEventListener('click',FormulariodeConctacto);
+document.addEventListener('DOMContentLoaded',FormulariodeConctacto);
